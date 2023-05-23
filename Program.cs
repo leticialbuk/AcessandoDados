6261174fc113c36a66506a0ae5b1d12a4bb98794
leticialbuk <leticia.albuk@gmail.com>
2023-05-22 21:22:32 -0300

    conectando ao banco de dados

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace AcessandoDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=base;User Id=sa;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
