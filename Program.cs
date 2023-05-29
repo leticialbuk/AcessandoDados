2aa6f2975f46132312320a65714f8c588324b97b
leticialbuk <leticia.albuk@gmail.com>
2023-05-29 15:58:22 -0300

    utilizando Parametros para inserçãcao de dados no SQL

--- Program.cs
+++ Program.cs
@@ -10,12 +10,44 @@ namespace AcessandoDados
         {
             const string connectionString = @"Server=localhost\SQLEXPRESS;Database=balta;Trusted_Connection=True;TrustServerCertificate=True";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços AWS";
+            category.Featured = false;
+
+            var insertSql = $@"INSERT INTO 
+                    [Category]      
+                VALUES(
+                    @Id,
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
 
