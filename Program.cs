4fbc7e1423f66d37b6f4e6d3e755ab483f165369
leticialbuk <leticia.albuk@gmail.com>
2023-05-31 18:55:20 -0300

    utilizando Many para adicionar e atulizar mais de um registro

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace AcessandoDados
 
             using (var connection = new SqlConnection(connectionString))
             {
-                UpdateCategory(connection);
+                //CreateManyCategory(connection);
+                //UpdateCategory(connection);
                 ListCategories(connection);
             }
         }
@@ -67,10 +68,68 @@ namespace AcessandoDados
             var rows = connection.Execute(updateQuery, new
             {
                 id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
-                title = "Frontend 2023"
+                title = "Frontend"
             });
 
             Console.WriteLine($"{rows} Registros atualizados!");
         }
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços AWS";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Nova categoria";
+            category2.Url = "categoria-nova";
+            category2.Summary = "Nova categoria";
+            category2.Order = 9;
+            category2.Description = "Categoria";
+            category2.Featured = true;
+
+            var insertSql = $@"INSERT INTO 
+                    [Category]      
+                VALUES(
+                    @Id,
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]{
+                new
+                {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+                },
+
+                new
+                {
+                category2.Id,
+                category2.Title,
+                category2.Url,
+                category2.Summary,
+                category2.Order,
+                category2.Description,
+                category2.Featured
+                }
+            });
+
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
     }
 }
