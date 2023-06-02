a0fd7c478fcfb790ee3c1041ab725dc5fe8a9f8c
leticialbuk <leticia.albuk@gmail.com>
2023-06-02 17:31:23 -0300

    adicionando Id com ExecuteScalar com dapper

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace AcessandoDados
                 //UpdateCategory(connection);
                 //ListCategories(connection);
                 //ExecuteProcedure(connection);
-                ExecuteReadProcedure(connection);
+                //ExecuteReadProcedure(connection);
+                ExecuteScalar(connection);
             }
         }
 
@@ -146,5 +147,39 @@ namespace AcessandoDados
                 Console.WriteLine(item.Title);
             }
         }
+        static void ExecuteScalar(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços AWS";
+            category.Featured = false;
+
+            var insertSql = $@"INSERT INTO 
+                    [Category]
+                OUTPUT inserted.[Id]      
+                VALUES(
+                    NEWID(),
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var id = connection.ExecuteScalar<Guid>(insertSql, new
+            {
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"A categoria inserida foi: {id}");
+        }
+
     }
 }
